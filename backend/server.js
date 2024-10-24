1e5dc58eb4ca658b8cf12fffcfc2d1a80c7d22e7
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-24 22:18:38 +0545

    complete payment route with stripe

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import authRoutes from './routes/auth.route.js'
 import productRoutes from './routes/product.route.js'
 import cartRoutes from './routes/cart.route.js'
 import couponRoutes from './routes/coupon.route.js'
+import paymentRoutes from './routes/payment.route.js'
 
 const app = express()
 
@@ -21,6 +22,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
+app.use("/api/payment", paymentRoutes)
 
 
 app.listen(PORT, () => {
