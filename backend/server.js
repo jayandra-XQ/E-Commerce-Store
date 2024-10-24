b8b701343685b2bf6fd469af5b3bbb05b50dc5ec
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-24 12:33:59 +0545

    completed Product Routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { connectDB } from './lib/db.js'
 
 
 import authRoutes from './routes/auth.route.js'
+import productRoutes from './routes/product.route.js'
 
 const app = express()
 
@@ -14,7 +15,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); //alows you to parse the body of the request
 app.use(cookieParser()); //parse cookies from request headers
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
