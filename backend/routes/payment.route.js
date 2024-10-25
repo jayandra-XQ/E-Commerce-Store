e84d502342e766f7923cbd8f7b8d2691a98781c1
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-25 12:56:19 +0545

    fix: module

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { protectRoute } from '../middleware/auth.middleware'
-import { checkoutSuccess, createCheckoutSession } from '../controllers/payment.controller'
+import { protectRoute } from '../middleware/auth.middleware.js'
+import { checkoutSuccess, createCheckoutSession } from '../controllers/payment.controller.js'
 
 const router = express.Router()
 
