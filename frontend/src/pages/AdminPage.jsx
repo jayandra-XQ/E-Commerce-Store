6d0fad8ae1c65c0d17e9ae70a97ec1bafd9a6b2a
Jayandra Hamal <salonhamal278@gmail.com>
2024-11-04 10:56:04 +0545

    Building Admin Dashboard

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,23 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BarChart, PlusCircle, ShoppingBasket, } from "lucide-react"
 import { motion } from "framer-motion";
 import CreateProductForm from '../components/CreateProductForm ';
 import ProductsList from '../components/ProductsList';
 import AnalyticsTab from '../components/AnalyticsTab';
+import { useProductStore } from '../stores/useProductStore';
 
 const tabs = [
-  { id: "create", label: "Create Product", icon: PlusCircle },
-  { id: "products", label: "Products", icon: ShoppingBasket },
-  { id: "analytics", label: "Analytics", icon: BarChart }
+	{ id: "create", label: "Create Product", icon: PlusCircle },
+	{ id: "products", label: "Products", icon: ShoppingBasket },
+	{ id: "analytics", label: "Analytics", icon: BarChart }
 ]
 
 const AdminPage = () => {
-  const [activeTab, setActiveTab] = useState("create")
+	const [activeTab, setActiveTab] = useState("create")
+	const { fetchAllProducts } = useProductStore()
 
-  return (
-    <div className='min-h-screen bg-gray-900 text-white relative overflow-hidden'>
-      <div className="container relative z-10 mx-auto px-4 py-16">
-      <motion.h1
+	useEffect(() => {
+		fetchAllProducts()
+	}, [fetchAllProducts])
+
+	return (
+		<div className='min-h-screen  relative overflow-hidden'>
+			<div className="container relative z-10 mx-auto px-4 py-16">
+				<motion.h1
 					className='text-4xl font-bold mb-8 text-emerald-400 text-center'
 					initial={{ opacity: 0, y: -20 }}
 					animate={{ opacity: 1, y: 0 }}
@@ -25,18 +31,17 @@ const AdminPage = () => {
 				>
 					Admin Dashboard
 				</motion.h1>
-        
 
-        <div className='flex justify-center mb-8'>
+
+				<div className='flex justify-center mb-8'>
 					{tabs.map((tab) => (
 						<button
 							key={tab.id}
 							onClick={() => setActiveTab(tab.id)}
-							className={`flex items-center px-4 py-2 mx-2 rounded-md transition-colors duration-200 ${
-								activeTab === tab.id
+							className={`flex items-center px-4 py-2 mx-2 rounded-md transition-colors duration-200 ${activeTab === tab.id
 									? "bg-emerald-600 text-white"
 									: "bg-gray-700 text-gray-300 hover:bg-gray-600"
-							}`}
+								}`}
 						>
 							<tab.icon className='mr-2 h-5 w-5' />
 							{tab.label}
@@ -44,13 +49,13 @@ const AdminPage = () => {
 					))}
 				</div>
 
-        {activeTab === "create" && <CreateProductForm />}
+				{activeTab === "create" && <CreateProductForm />}
 				{activeTab === "products" && <ProductsList />}
 				{activeTab === "analytics" && <AnalyticsTab />}
 
-      </div>
-    </div>
-  )
+			</div>
+		</div>
+	)
 }
 
 export default AdminPage
--- frontend/src/stores/useProductStore.js
+++ frontend/src/stores/useProductStore.js
@@ -22,5 +22,51 @@ export const useProductStore = create((set) => ({
       toast.error("Error creating product")
       set({ loading: false })
     }
+  },
+
+  fetchAllProducts: async () => {
+    set({ loading: true })
+    try {
+      const res = await axios.get("/products")
+      set({ products: res.data.products, loading: false })
+    } catch (error) {
+      toast.error("Error fetching products")
+      set({ loading: false })
+    }
+  },
+
+  
+  deleteProduct: async (productId) => {
+		set({ loading: true });
+		try {
+			await axios.delete(`/products/${productId}`);
+			set((prevProducts) => ({
+				products: prevProducts.products.filter((product) => product._id !== productId),
+				loading: false,
+			}));
+		} catch (error) {
+			set({ loading: false });
+			toast.error(error.response.data.error || "Failed to delete product");
+		}
+	},
+
+
+  toggleFeaturedProduct: async (productId) => {
+    set({loading: true});
+
+    try {
+      const res = await axios.patch(`/products/${productId}`);
+
+      // this will update the isFeatured prop of the product
+      set((prevProducts) => ({
+        products: prevProducts.products.map((product) =>
+          product._id === productId ? {...product, isFeatured: res.data.isFeatured} : product
+        ),
+        loading: false,
+      }))
+    } catch (error) {
+      toast.error("Error toggling featured product");
+      set({loading: false});
+    }
   }
 }))
